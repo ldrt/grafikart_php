a3081914db4335922a57e560bc29b1bfa2766e93
Pattounes <77328374+ldrt@users.noreply.github.com>
2021-02-24 09:55:00 +0100

    Cours: Variables

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php 
+    $nom = 'Doe';
+    $prenom = 'John';
+
+    $note1 = 10;
+    $note2 = 20;
+    $moyenne = ($note1 + $note2) / 2;
+
+    echo "Bonjour $prenom $nom vous avez eu $moyenne de moyenne";
+?>
