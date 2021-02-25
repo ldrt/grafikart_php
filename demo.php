2616917057e5ba85bbe61dfd87b6c35a763f5834
Pattounes <77328374+ldrt@users.noreply.github.com>
2021-02-25 11:09:22 +0100

    Cours: Fonctions

--- demo.php
+++ demo.php
@@ -1,44 +1,44 @@
 <?php 
-$horaires = [];
-$ouverture = null;
-$fermeture = null;
-$input = null;
-$ouvert = null;
-while (true) {
-    $ouverture = (int) readline('Ouverture : ');
-    $fermeture = (int) readline('Fermeture : ');
-    while ($fermeture <= $ouverture) {
-        $fermeture = (int) readline('Fermeture : ');
-        echo "Le magasin fermé après l\'ouverture. \n";
-    }
-    $horaires[] = [
-        "ouverture" => $ouverture,
-        "fermeture" => $fermeture
-    ];
-    $nouveauCreneau = (string) readline('Nouveau creneau ? (o/n) ');
-    if ($nouveauCreneau === 'n') {
-        break;
+function repondre ($question) {
+    while (true) {
+        $reponse = readline($question . "(o)ui/(n)on : ");
+        if($reponse === 'o') {
+            return true;
+        } elseif ($reponse === 'n') {
+            return false;
+        }
     }
 }
 
-echo 'Le magasin est ouvert de ';
-foreach ($horaires as $i => $horaire) {
-    if ($i > 0 ) {
-        echo ' et de ';
+function demander_creneaux ($phrase = 'Veuillez entrer un creneau') {
+    while (true) {
+        echo $phrase . "\n";
+        while (true) {
+            $ouverture = (int) readline('Ouverture : ');
+            // vérification: heure valide
+            if ($ouverture >=0 && $ouverture <=23) {
+                break;
+            }
+        }
+        while (true) {
+            $fermeture = (int) readline('Fermeture : ');
+            // vérification: heure valide et fermeture après ouverture
+            if ($fermeture >=0 && $fermeture <=23 && $fermeture > $ouverture) {
+                break;
+            }
+        }
+        $horaires[] = [
+            "ouverture" => $ouverture,
+            "fermeture" => $fermeture
+        ];
+        // vérification: nouveau créneau
+        $continuer = repondre('Continuer ?');
+        if (!$continuer) {
+            return $horaires;
+        }
     }
-    echo "{$horaire['ouverture']}h à {$horaire['fermeture']}h";
 }
 
-$input = readline("\n Ouvert ? ");
-foreach ($horaires as $horaire) {
-    if ($input >= $horaire['ouverture'] && $input <= $horaire['fermeture']) {
-        $ouvert = true;
-        break;
-    }
-}
-if ($ouvert) {
-    echo 'Ouvert';
-} else {
-    echo 'Fermé';
-}
+$creneaux = demander_creneaux();
+var_dump($creneaux);
 ?>
