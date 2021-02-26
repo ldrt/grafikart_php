9a6ee640c5b2fea320e790bf294d34ec5a4f06e0
Pattounes <77328374+ldrt@users.noreply.github.com>
2021-02-26 10:09:59 +0100

    Cours: Dates

--- functions_site.php
+++ functions_site.php
@@ -16,4 +16,33 @@ function nav_menu (string $linkClass ='') : string {
         nav_item('/index.php', 'Home', $linkClass) . 
         nav_item('/contact.php', 'Contact', $linkClass);
 }
+
+function select (string $name, $value, array $options) : string {
+    $html_options = [];
+    foreach ($options as $key => $option) {
+        $attributes = $key == $value ? 'selected' : '';
+        $html_options[] = "<option value='$key' $attributes>$option</option>";
+    }
+    return "<select name='$name' class='form-control'>" . implode($html_options) . '</select>';
+}
+
+function creneaux_html (array $creneaux) : string {
+    if (empty($creneaux)) {
+        return 'Fermé';
+    }
+    $h = [];
+    foreach ($creneaux as $creneau) {
+        $h[] = "de {$creneau[0]}h à {$creneau[1]}h ";
+    }
+    return 'Ouvert ' . implode(' et ', $h);
+}
+
+function in_creneaux (int $heure, array $creneaux) : bool {
+    foreach ($creneaux as $creneau) {
+        if ($heure >= $creneau[0] && $heure < $creneau[1]) {
+            return true;
+        }
+    }
+    return false;
+}
 ?>
